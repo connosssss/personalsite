f872c30db3e8bb0ad9b13f6d5e56d4d8f9b64c98
connosssss <connorgriffin2424@gmail.com>
2026-07-24 03:25:43 -0700

    #3

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,16 @@
+try {
+  const stat = await Deno.stat("./_fresh").catch(() => null);
+  if (!stat) {
+    console.log("Running deno task build for Vercel...");
+    const cmd = new Deno.Command(Deno.execPath(), {
+      args: ["task", "build"],
+    });
+    await cmd.output();
+  }
+} catch (err) {
+  console.log("Pre-build check:", err);
+}
+
 import { start } from "$fresh/server.ts";
 import manifest from "../fresh.gen.ts";
 import config from "../fresh.config.ts";
