14d00d3d4e0bd4c443325d2de1ab19ad881f2214
connosssss <connorgriffin2424@gmail.com>
2026-07-24 03:21:17 -0700

    #2

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,5 @@
-import { createHandler } from "$fresh/server.ts";
+import { start } from "$fresh/server.ts";
 import manifest from "../fresh.gen.ts";
 import config from "../fresh.config.ts";
 
-const handlerPromise = createHandler(manifest, config);
-
-export default async function (req: Request): Promise<Response> {
-  const handler = await handlerPromise;
-  return await handler(req);
-}
+await start(manifest, config);
