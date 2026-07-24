b08fdbec7c4f0467e91b8d8b8c14c7903ff35d85
connosssss <connorgriffin2424@gmail.com>
2026-07-24 03:14:31 -0700

    fix to index

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,5 @@
-import { Server } from "$fresh/server.ts";
+import { createHandler } from "$fresh/server.ts";
 import manifest from "../fresh.gen.ts";
 import config from "../fresh.config.ts";
 
-const server = new Server(manifest, config);
-const handler = server.requestHandler();
-
-export default async function (req: Request) {
-  return await handler(req);
-}
+export default await createHandler(manifest, config);
