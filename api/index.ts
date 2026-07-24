2f0b65af4a45e603bc6d22ced85481e30d23723d
connosssss <connorgriffin2424@gmail.com>
2026-07-24 03:17:21 -0700

    correct build #1

--- api/index.ts
+++ api/index.ts
@@ -2,4 +2,9 @@ import { createHandler } from "$fresh/server.ts";
 import manifest from "../fresh.gen.ts";
 import config from "../fresh.config.ts";
 
-export default await createHandler(manifest, config);
+const handlerPromise = createHandler(manifest, config);
+
+export default async function (req: Request): Promise<Response> {
+  const handler = await handlerPromise;
+  return await handler(req);
+}
